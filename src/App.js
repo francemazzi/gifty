894fe00222c249f8940a9ac48e0f2f031882241c
FraSma <francemazzi@gmail.com>
2016-01-16 18:05:39 +0100

    setup React con Create React App

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/globals.css';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <h1>Gifty</h1>
+    <p>Organizza le tue idee regalo.</p>
+  </div>
+);
+
+export default App;
