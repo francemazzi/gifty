cbfd1534e4e447eb83309fc90c6b8c707c46c6b3
FraSma <francemazzi@gmail.com>
2022-10-25 17:28:34 +0200

    delete function

--- src/Components/GiftItem/index.tsx
+++ src/Components/GiftItem/index.tsx
@@ -1,6 +1,7 @@
 import "./GitftItem.css";
 import { Gift } from "../../model";
 import React from "react";
+import GiftsList from "../Giftslist";
 
 interface Props {
   item: Gift;
@@ -9,10 +10,18 @@ interface Props {
 }
 
 const GiftItem = ({ item, gifts, setGifts }: Props) => {
+  //delete function
+  const handleDelete = (id: number) => {
+    setGifts(gifts.filter((item) => item.id !== id));
+  };
   return (
     <div className="giftCard">
       <form className="giftCard__single">
         <span className="giftCar__single--text">{item.gift}</span>
+        <button className="buttonIcon">📝</button>
+        <button className="buttonIcon" onClick={() => handleDelete(item.id)}>
+          ❌
+        </button>
       </form>
     </div>
   );
