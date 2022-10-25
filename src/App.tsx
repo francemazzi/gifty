b63b4e0f85920eac88ef39b55668f0e90eba2dca
FraSma <francemazzi@gmail.com>
2022-10-25 09:30:06 +0200

    add gift - setgift

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
+import { useState } from "react";
 import Inputfield from "./Components/Inputfield";
 import HeaderUno from "./Components/Header";
 
 const App: React.FC = () => {
+  const [gift, setGift] = useState<string>("");
+
   return (
     <div className="App">
       <HeaderUno></HeaderUno>
-      <Inputfield></Inputfield>
+      <Inputfield gift={gift} setTodo={setGift}></Inputfield>
     </div>
   );
 };
--- src/Components/Inputfield/index.tsx
+++ src/Components/Inputfield/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 import "./Inputfield.css";
 
-const Inputfield = () => {
+interface Props {
+  gift: string;
+  setGift: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const Inputfield: React.FC<Props> = ({ gift, setGift }: Props) => {
   return (
-    <>
-      <div>
-        <form className="input">
-          <input
-            className="input__box"
-            type="input"
-            placeholder="Inserisci un tuo desiderio..."
-          />
-          <button className="input__submit">Proponi</button>
-        </form>
-      </div>
-    </>
+    <div>
+      <form className="input">
+        <input
+          value={gift}
+          onChange={() => setGift(e.target.value)}
+          className="input__box"
+          type="input"
+          placeholder="Inserisci un tuo desiderio..."
+        />
+        <button className="input__submit">Proponi</button>
+      </form>
+    </div>
   );
 };
 
