16fb98686b57b02c500032850205c17ac4455469
FraSma <francemazzi@gmail.com>
2022-10-25 21:13:48 +0200

    errore modify da cliccare due volte per modifica

--- src/Components/GiftItem/index.tsx
+++ src/Components/GiftItem/index.tsx
@@ -1,7 +1,7 @@
 import "./GitftItem.css";
 import { Gift } from "../../model";
 import React from "react";
-import GiftsList from "../Giftslist";
+import { useState } from "react";
 
 interface Props {
   item: Gift;
@@ -14,11 +14,62 @@ const GiftItem = ({ item, gifts, setGifts }: Props) => {
   const handleDelete = (id: number) => {
     setGifts(gifts.filter((item) => item.id !== id));
   };
+
+  //edit area
+
+  // edit
+  const [edit, setEdit] = useState<Boolean>(false);
+  // edit dentro a card
+  const [editGift, setEditgift] = useState<string>(item.gift);
+  //edit function
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setGifts(
+      gifts.map((item) => (item.id === id ? { ...item, gift: editGift } : item))
+    );
+
+    // setEdit(false);
+  };
+  const handleOk = () => {
+    setEdit(false);
+  };
+
   return (
     <div className="giftCard">
-      <form className="giftCard__single">
-        <span className="giftCar__single--text">{item.gift}</span>
-        <button className="buttonIcon">📝</button>
+      <form
+        className="giftCard__single"
+        onSubmit={(e) => handleEdit(e, item.id)}
+      >
+        {edit ? (
+          <div>
+            <input
+              value={editGift}
+              onChange={(e) => setEditgift(e.target.value)}
+              className="editCard"
+            />
+
+            <button
+              value={editGift}
+              className="buttonIcon"
+              onClick={() => handleOk()}
+            >
+              ✅
+            </button>
+          </div>
+        ) : (
+          <span className="giftCar__single--text">{item.gift}</span>
+        )}
+
+        <button
+          className="buttonIcon"
+          onClick={() => {
+            if (!edit && !item.delete) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          📝
+        </button>
         <button className="buttonIcon" onClick={() => handleDelete(item.id)}>
           ❌
         </button>
