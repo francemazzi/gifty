10d6a4877488f00759703b4654202436b8eb575e
FraSma <francemazzi@gmail.com>
2022-11-03 00:43:00 +0100

    db gifts

---
+++ src/services/gift.services.ts
@@ -0,0 +1,36 @@
+import { db } from "../../backend/firebase";
+import {
+  collection,
+  getDocs,
+  getDoc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
+
+// costante iniziarele
+const giftCollectionRef = collection(db, "gifts");
+class GiftDataService {
+  addGifts = (newGift: any) => {
+    return addDoc(giftCollectionRef, newGift);
+  };
+  updateGift = (id: string, updateGift: any) => {
+    //check se abbiamo gift o no dentro a collection
+    const giftDoc = doc(db, "gifts", id);
+    return updateDoc(giftDoc, updateGift);
+  };
+  deleteGift = (id: string) => {
+    const giftDoc = doc(db, "gifts", id);
+    return deleteDoc(giftDoc);
+  };
+  getAllGifts = () => {
+    return getDocs(giftCollectionRef);
+  };
+  getGift = (id: string) => {
+    const giftDoc = doc(db, "books", id);
+    return getDoc(giftDoc);
+  };
+}
+
+export default new GiftDataService();
